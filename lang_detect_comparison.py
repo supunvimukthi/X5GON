1a2158f88e0492901666202eb7accf353350040a
supunvimukthi <supun.15@cse.mrt.ac.lk>
2020-04-04 15:48:24 +0530

    added fasttext

--- lang_detect_comparison.py
+++ lang_detect_comparison.py
@@ -5,20 +5,26 @@ from textblob import TextBlob
 from polyglot.text import Text, Word
 from langdetect import detect, DetectorFactory, detect_langs
 from guess_language import guess_language
+import fasttext
+from pycountry import languages
+import sys
+# reload(sys)
+# sys.setdefaultencoding('UTF8') # default encoding to utf-8
+
 
 # method: textBlob
 # Dedect which language used
 # @return: True
 # @completed
 def textBlob():
-	with open("./yourinputfile.txt", "r") as sentences:
+	with open("./input_text.txt", "r") as sentences:
 		try:
 			for sentence in sentences:
 				sentence = sentence.strip()
 				blob = TextBlob(sentence)
 				result = blob.detect_language()
-				with open("./output/textBlobResult.short.txt", "w") as output:
-					output.write(sentence, ":", result)
+				print(sentence, ":", result,file=open("./output/textBlobResult.short.txt","a"))
+
 		except:
 			pass
 	return True
@@ -29,7 +35,7 @@ def textBlob():
 # @completed
 def polyglot():
 	#: Import dataset
-	with open("./yourinputfile.txt", "r") as sentences:
+	with open("./input_text.txt", "r") as sentences:
 		try:
 			for sentence in sentences:
 				sentence = sentence.strip()
@@ -46,7 +52,7 @@ def polyglot():
 # @completed
 def chardetExport():
 	#: Import dataset
-	with open("./yourinputfile.txt", "r") as sentences:
+	with open("./input_text.txt", "r") as sentences:
 		try:
 			for sentence in sentences:
 				sentence = sentence.strip()
@@ -63,7 +69,7 @@ def chardetExport():
 # @completed
 def langDedect():
 	#: Import dataset
-	with open("./yourinputfile.txt", "r") as sentences:
+	with open("./input_text.txt", "r") as sentences:
 		try:
 			for sentence in sentences:
 				sentence = sentence.strip()
@@ -80,7 +86,7 @@ def langDedect():
 # @completed
 def guessLanguage():
 	#: Import dataset
-	with open("./yourinputfile.txt", "r") as sentences:
+	with open("./input_text.txt", "r") as sentences:
 		for sentence in sentences:
 			try:
 				sentence = sentence.strip()
@@ -97,7 +103,7 @@ def guessLanguage():
 # @completed
 def langidExport():
 	#: Import dataset
-	with open("./yourinputfile.txt", "r") as sentences:
+	with open("./input_text.txt", "r") as sentences:
 		for sentence in sentences:
 			try:
 				sentence = sentence.strip()
@@ -108,6 +114,26 @@ def langidExport():
 				pass
 	return True
 
+lid_model = fasttext.load_model("./lid.176.ftz")
+
+def fasttextExport():
+	#: Import dataset
+	with open("./input_text.txt", "r") as sentences:
+		for sentence in sentences:
+			try:
+				sentence=sentence.split("\n")[0]
+				result = lid_model.predict([sentence])
+				print(sentence, ":" ,result, file = open("./output/fastTextResult.short.txt", "a"))
+			except Exception as e:
+				print(e)
+				pass
+	return True
+
+
+
+language_name = languages.get(alpha_2='fr').name
+print(language_name)
+
 
 if __name__ == '__main__':
 	textBlob()
@@ -116,3 +142,4 @@ if __name__ == '__main__':
 	langDedect()
 	guessLanguage()
 	langidExport()
+	fasttextExport()
