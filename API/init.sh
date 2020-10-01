d45c3adf5efcbbd2f82725a3f7fe16d00d0e715c
Supun Ranwaka <supun.15@cse.mrt.ac.lk>
2020-10-01 11:33:46 +0530

    lang detect api init file changed

--- API/init.sh
+++ API/init.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 sudo apt install python3-venv
-python3.6 -m venv lang_detect_api
-source lang_detect_api/bin/activate
-export PYTHON_PATH="/mnt/c/Users/Supun/Documents/X5GON/API/lang_detect_api/bin"
+python3.6 -m venv lang_detect
+source lang_detect/bin/activate
+export REPO_PATH="/mnt/c/Users/Supun/Documents" # change this path to your repo directory
+export PYTHON_PATH=$REPO_PATH"/X5_langdetect/lang_detect/bin"
+export FASTTEXT_MODEL_DIR=$REPO_PATH"/X5_langdetect/x5gon_rest/_data"
 export LANGUAGE_API_URL="http://127.0.0.1:5000/language_detection"
-export FASTTEXT_MODEL_DIR="/mnt/c/Users/Supun/Documents/X5GON/API/x5gon_rest/_data" # path to repo +  X5_langdetect/x5gon_rest/_data
 sudo apt-get update
 sudo apt-get install libpq-dev -y
 CFLAGS="-Wno-narrowing" pip3 install cld2-cffi
